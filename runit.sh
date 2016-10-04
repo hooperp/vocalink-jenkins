472ed64fdd9936aa3eca8dff0841234ab00357b2
Paul Hooper <phooper0001@yahoo.com>
2016-10-04 15:13:34 +0100

    first commit

---
+++ runit.sh
@@ -0,0 +1,5 @@
+/Users/paulhooper/Desktop/rancher-compose-v0.8.6/rancher-compose  --project-name vocalink-demo                          \
+                                                                  --url http://52.30.88.162:8080/v1/                    \
+                                                                  --access-key 10F4D2EBCE6DB1B78F5F                     \
+                                                                  --secret-key ACo6j4PDA7Uju2Husmp8v7q93vcchk6exE8BoiqH \
+                                                                  --verbose create 
